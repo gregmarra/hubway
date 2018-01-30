500dabf4bb8b824d0ab2887e8eb7aa9b768e8680
Greg Stanton Marra <greg.marra@gmail.com>
2018-01-29 19:01:05 -0500

    Use lubridate, look at time/date distributions.

--- first_attempt.R
+++ first_attempt.R
@@ -1,10 +1,14 @@
 # a first attempt at loading and analyzing data with R.
 # gregmarra, Jan 2017
 
-library(dplyr)
+library(tidyverse)
 library(ggplot2)
+library(lubridate)
 
-trips <- read.csv('data/201706-hubway-tripdata.csv', stringsAsFactors=FALSE)
+trips <- read.csv('data/201706-hubway-tripdata.csv', stringsAsFactors=FALSE) %>%
+  as_tibble()
+
+# Functions
 
 addAges <- function(df) {
   return (df %>%
@@ -12,6 +16,18 @@ addAges <- function(df) {
     mutate(age = 2017 - as.numeric(birth.year)) %>%
     filter(age < 100))
 }
+
+addTimes <- function(df) {
+  return (df %>%
+    mutate(
+      starttime = ymd_hms(starttime),
+      stoptime = ymd_hms(stoptime),
+    ))
+}
+
+# Poking at Data
+
+## Rides by Age
 
 trips %>%
   addAges() %>%
@@ -33,5 +49,51 @@ trips %>%
     title = "Young Riders Dominate",
     subtitle = "Hubway is most used by people between the ages of 20 and 40"
   )
-ggsave("./output/rides_by_age.pdf")
+ggsave("rides_by_age.pdf")
+
+## Time of Day
+
+trips %>%
+  addTimes() %>%
+  mutate(
+    start_hour = update(starttime, yday = 1),
+    stop_hour = update(stoptime, yday = 1)
+  ) %>%
+  ggplot(aes(
+    x = start_hour
+  )) +
+  geom_histogram(
+    bins = 192
+  )
+
+## Day of Week
+trips %>%
+  addTimes() %>%
+  mutate(
+    day_of_week = wday(starttime, label = TRUE)
+  ) %>%
+  ggplot(aes(
+    x = day_of_week
+  )) +
+  geom_bar()
+
+trips %>%
+  addTimes() %>%
+  mutate(
+    start_hour = update(starttime, yday = 1),
+    stop_hour = update(stoptime, yday = 1),
+    day_of_week = wday(starttime, label = TRUE)
+  ) %>%
+  ggplot(aes(
+    x = start_hour,
+    color = day_of_week
+  )) +
+  geom_freqpoly(bins = 48) +
+  labs(
+    x = "Trip Start",
+    y = "Number of Rides",
+    title = "Weekends and Weekdays Differ",
+    subtitle = "Weekdays show clear commute spikes; weekends higher afternoon usage"
+  )
+ggsave("times_by_day_of_week.pdf")
 
